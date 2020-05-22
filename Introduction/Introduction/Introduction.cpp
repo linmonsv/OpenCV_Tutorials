c8ab69a5171c577840bed3889afb01c2b38cb3be
qfx <qin2@qq.com>
2020-05-22 14:29:24 +0800

    add Introduction

---
+++ Introduction/Introduction/Introduction.cpp
@@ -0,0 +1,35 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc/imgproc.hpp>     // cv::Canny()
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <string>
+using namespace cv;
+using namespace std;
+int main(int argc, char** argv)
+{
+    String imageName("HappyFish.jpg"); // by default
+    if (argc > 1)
+    {
+        imageName = argv[1];
+    }
+    Mat image;
+    image = imread(imageName, IMREAD_COLOR); // Read the file
+    if (image.empty())                      // Check for invalid input
+    {
+        cout << "Could not open or find the image" << std::endl;
+        return -1;
+    }
+    //namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
+    //imshow("Display window", image);                // Show our image inside it.
+    //waitKey(0); // Wait for a keystroke in the window
+
+    Mat edges;
+    Canny(image, edges, 10, 100);
+
+    Mat gray_image;
+    cvtColor(image, gray_image, COLOR_BGR2GRAY);
+    //imwrite("Gray_Image.jpg", gray_image);
+
+    return 0;
+}
