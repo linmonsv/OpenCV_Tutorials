c79724fc8c5bf4ada177ad0a6939c86e1926431f
qfx <qin2@qq.com>
2020-06-09 10:25:52 +0800

    need surf add Feature_Matching_with_FLANN.cpp Features2D_Homography_to_find_a_known_object.cpp

--- Features_2D_framework/Feature_Matching_with_FLANN/Feature_Matching_with_FLANN.cpp
+++ Features_2D_framework/Feature_Matching_with_FLANN/Feature_Matching_with_FLANN.cpp
@@ -1,20 +1,63 @@
-// Feature_Matching_with_FLANN.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+#include "opencv2/core.hpp"
+#ifdef HAVE_OPENCV_XFEATURES2D
+#include "opencv2/highgui.hpp"
+#include "opencv2/features2d.hpp"
+#include "opencv2/xfeatures2d.hpp"
+using namespace cv;
+using namespace cv::xfeatures2d;
+using std::cout;
+using std::endl;
+const char* keys =
+"{ help h |                          | Print help message. }"
+"{ input1 | ../data/box.png          | Path to input image 1. }"
+"{ input2 | ../data/box_in_scene.png | Path to input image 2. }";
+int main(int argc, char* argv[])
+{
+    CommandLineParser parser(argc, argv, keys);
+    Mat img1 = imread(parser.get<String>("input1"), IMREAD_GRAYSCALE);
+    Mat img2 = imread(parser.get<String>("input2"), IMREAD_GRAYSCALE);
+    if (img1.empty() || img2.empty())
+    {
+        cout << "Could not open or find the image!\n" << endl;
+        parser.printMessage();
+        return -1;
+    }
+    //-- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
+    int minHessian = 400;
+    Ptr<SURF> detector = SURF::create(minHessian);
+    std::vector<KeyPoint> keypoints1, keypoints2;
+    Mat descriptors1, descriptors2;
+    detector->detectAndCompute(img1, noArray(), keypoints1, descriptors1);
+    detector->detectAndCompute(img2, noArray(), keypoints2, descriptors2);
+    //-- Step 2: Matching descriptor vectors with a FLANN based matcher
+    // Since SURF is a floating-point descriptor NORM_L2 is used
+    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
+    std::vector< std::vector<DMatch> > knn_matches;
+    matcher->knnMatch(descriptors1, descriptors2, knn_matches, 2);
+    //-- Filter matches using the Lowe's ratio test
+    const float ratio_thresh = 0.7f;
+    std::vector<DMatch> good_matches;
+    for (size_t i = 0; i < knn_matches.size(); i++)
+    {
+        if (knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance)
+        {
+            good_matches.push_back(knn_matches[i][0]);
+        }
+    }
+    //-- Draw matches
+    Mat img_matches;
+    drawMatches(img1, keypoints1, img2, keypoints2, good_matches, img_matches, Scalar::all(-1),
+        Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+    //-- Show detected matches
+    imshow("Good Matches", img_matches);
+    waitKey();
+    return 0;
+}
+#else
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "This tutorial code needs the xfeatures2d contrib module to be run." << std::endl;
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+#endif
--- Features_2D_framework/Features2D_Homography_to_find_a_known_object/Features2D_Homography_to_find_a_known_object.cpp
+++ Features_2D_framework/Features2D_Homography_to_find_a_known_object/Features2D_Homography_to_find_a_known_object.cpp
@@ -1,20 +1,92 @@
-// Features2D_Homography_to_find_a_known_object.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+#include "opencv2/core.hpp"
+#ifdef HAVE_OPENCV_XFEATURES2D
+#include "opencv2/calib3d.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/features2d.hpp"
+#include "opencv2/xfeatures2d.hpp"
+using namespace cv;
+using namespace cv::xfeatures2d;
+using std::cout;
+using std::endl;
+const char* keys =
+"{ help h |                          | Print help message. }"
+"{ input1 | ../data/box.png          | Path to input image 1. }"
+"{ input2 | ../data/box_in_scene.png | Path to input image 2. }";
+int main(int argc, char* argv[])
+{
+    CommandLineParser parser(argc, argv, keys);
+    Mat img_object = imread(parser.get<String>("input1"), IMREAD_GRAYSCALE);
+    Mat img_scene = imread(parser.get<String>("input2"), IMREAD_GRAYSCALE);
+    if (img_object.empty() || img_scene.empty())
+    {
+        cout << "Could not open or find the image!\n" << endl;
+        parser.printMessage();
+        return -1;
+    }
+    //-- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
+    int minHessian = 400;
+    Ptr<SURF> detector = SURF::create(minHessian);
+    std::vector<KeyPoint> keypoints_object, keypoints_scene;
+    Mat descriptors_object, descriptors_scene;
+    detector->detectAndCompute(img_object, noArray(), keypoints_object, descriptors_object);
+    detector->detectAndCompute(img_scene, noArray(), keypoints_scene, descriptors_scene);
+    //-- Step 2: Matching descriptor vectors with a FLANN based matcher
+    // Since SURF is a floating-point descriptor NORM_L2 is used
+    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
+    std::vector< std::vector<DMatch> > knn_matches;
+    matcher->knnMatch(descriptors_object, descriptors_scene, knn_matches, 2);
+    //-- Filter matches using the Lowe's ratio test
+    const float ratio_thresh = 0.75f;
+    std::vector<DMatch> good_matches;
+    for (size_t i = 0; i < knn_matches.size(); i++)
+    {
+        if (knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance)
+        {
+            good_matches.push_back(knn_matches[i][0]);
+        }
+    }
+    //-- Draw matches
+    Mat img_matches;
+    drawMatches(img_object, keypoints_object, img_scene, keypoints_scene, good_matches, img_matches, Scalar::all(-1),
+        Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+    //-- Localize the object
+    std::vector<Point2f> obj;
+    std::vector<Point2f> scene;
+    for (size_t i = 0; i < good_matches.size(); i++)
+    {
+        //-- Get the keypoints from the good matches
+        obj.push_back(keypoints_object[good_matches[i].queryIdx].pt);
+        scene.push_back(keypoints_scene[good_matches[i].trainIdx].pt);
+    }
+    Mat H = findHomography(obj, scene, RANSAC);
+    //-- Get the corners from the image_1 ( the object to be "detected" )
+    std::vector<Point2f> obj_corners(4);
+    obj_corners[0] = Point2f(0, 0);
+    obj_corners[1] = Point2f((float)img_object.cols, 0);
+    obj_corners[2] = Point2f((float)img_object.cols, (float)img_object.rows);
+    obj_corners[3] = Point2f(0, (float)img_object.rows);
+    std::vector<Point2f> scene_corners(4);
+    perspectiveTransform(obj_corners, scene_corners, H);
+    //-- Draw lines between the corners (the mapped object in the scene - image_2 )
+    line(img_matches, scene_corners[0] + Point2f((float)img_object.cols, 0),
+        scene_corners[1] + Point2f((float)img_object.cols, 0), Scalar(0, 255, 0), 4);
+    line(img_matches, scene_corners[1] + Point2f((float)img_object.cols, 0),
+        scene_corners[2] + Point2f((float)img_object.cols, 0), Scalar(0, 255, 0), 4);
+    line(img_matches, scene_corners[2] + Point2f((float)img_object.cols, 0),
+        scene_corners[3] + Point2f((float)img_object.cols, 0), Scalar(0, 255, 0), 4);
+    line(img_matches, scene_corners[3] + Point2f((float)img_object.cols, 0),
+        scene_corners[0] + Point2f((float)img_object.cols, 0), Scalar(0, 255, 0), 4);
+    //-- Show detected matches
+    imshow("Good Matches & Object detection", img_matches);
+    waitKey();
+    return 0;
+}
+#else
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "This tutorial code needs the xfeatures2d contrib module to be run." << std::endl;
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+#endif
